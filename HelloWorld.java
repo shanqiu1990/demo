e831f31307ed0de2c57442fcd8884c362bcde46b
qiushan <459567063@qq.com>
2020-05-09 18:18:21 +0800

    fix 2

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HellowWorld{
+
+public satic void main(String[] args){
+
+System.out.println("你555好吗")
+System.out.println("你好吗")
+System.out.println("你222222222好吗")
+System.out.println("你4444444444好吗")
+System.out.println("你好222吗")
+System.out.println("你55555555555好吗")
+System.out.println("你22好吗")
+
+}
+}
